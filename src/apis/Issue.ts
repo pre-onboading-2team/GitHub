23708261259c833a55096d425737a8d5ab73a8fe
MoSangBin <mosangbin94@gmail.com>
2022-10-30 05:08:43 +0900

    Refactor: getIssues api 로직 수정

--- src/apis/Issue.ts
+++ src/apis/Issue.ts
@@ -1,15 +1,27 @@
+import { IssueActionTypes } from "../contexts";
 import { Issue } from "../types";
 import { axiosInstance } from "../utils";
 
-export const getIssueList = async (): Promise<Issue[]> => {
-  const { data } = await axiosInstance.get(
-    "/issues?sort=comments&page=1&per_page=10",
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_GITHUB_API_TOKEN}`,
-      },
-    }
-  );
+export const getIssues = async (
+  dispatch: React.Dispatch<{
+    type: IssueActionTypes;
+    data?: Issue[];
+  }>,
+  page: number
+): Promise<void> => {
+  dispatch({ type: IssueActionTypes.GET_ISSUES_LOADING });
+  try {
+    const { data } = await axiosInstance.get(
+      `/issues?sort=comments&page=${page}&per_page=10`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_GITHUB_API_TOKEN}`,
+        },
+      }
+    );
 
-  return data;
+    dispatch({ type: IssueActionTypes.GET_ISSUES_SUCCESS, data });
+  } catch {
+    dispatch({ type: IssueActionTypes.GET_ISSUES_ERROR });
+  }
 };
