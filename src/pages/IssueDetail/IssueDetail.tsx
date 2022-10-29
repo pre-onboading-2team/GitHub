125f5acddf242c76584a6b94bbe7c1178824e6ea
MoSangBin <mosangbin94@gmail.com>
2022-10-30 06:33:21 +0900

    Feat: Issue 상세 페이지 api 로직 연결

--- src/pages/IssueDetail/IssueDetail.tsx
+++ src/pages/IssueDetail/IssueDetail.tsx
@@ -1,9 +1,58 @@
-import { useParams } from "react-router-dom";
+/* eslint-disable react/no-unescaped-entities */
+import { useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { getIssueDetail } from "../../apis";
+import { IssueCard, LoadingSpinner } from "../../components";
+import { useIssueDispatch, useIssueSelector } from "../../contexts";
+import { NotFound } from "../NotFound";
+import * as S from "./issueDetail.style";
 
 const IssueDetail = () => {
   const params = useParams();
-  console.log(params.id);
-  return <div>IssueDetail</div>;
+  const navigate = useNavigate();
+  const dispatch = useIssueDispatch();
+  const { issueDetail, isError, isLoading } = useIssueSelector();
+
+  useEffect(() => {
+    getIssueDetail(dispatch, Number(params.id));
+
+    if (isError) navigate("/not-found");
+  }, [dispatch, params, isError, navigate]);
+
+  if (isLoading) {
+    return (
+      <S.LoadingContainer>
+        <LoadingSpinner />
+      </S.LoadingContainer>
+    );
+  }
+
+  if (issueDetail) {
+    return (
+      <S.Container>
+        <S.IssueInfos>
+          <S.ImgContainer>
+            <img src={issueDetail.user.avatar_url} alt="profile" />
+          </S.ImgContainer>
+          <IssueCard
+            issueNumber={issueDetail.number}
+            title={issueDetail.title}
+            createdAt={issueDetail.created_at}
+            comments={issueDetail.comments as number}
+            writerName={issueDetail.user.login}
+            isDetailPage
+          />
+        </S.IssueInfos>
+        <S.Main>
+          <ReactMarkdown>{issueDetail.body}</ReactMarkdown>
+        </S.Main>
+      </S.Container>
+    );
+  }
+
+  return <NotFound />;
 };
 
 export default IssueDetail;
