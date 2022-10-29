b9739fb7610de78b398a0eb71bd43abdbac13597
MoSangBin <mosangbin94@gmail.com>
2022-10-30 01:32:58 +0900

    Refactor: Issue, User 공통 타입 분리

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,15 @@
-export {};
+export interface Issue {
+  id: number;
+  number: number;
+  user: User;
+  title: string;
+  body: string;
+  created_at: Date;
+  comments: number;
+}
+
+export interface User {
+  id: number;
+  login: string;
+  avatar_url: string;
+}
