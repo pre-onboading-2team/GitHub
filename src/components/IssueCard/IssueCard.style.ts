b275426103853b45deed49b1bb8e92786ca1a474
MoSangBin <mosangbin94@gmail.com>
2022-10-30 01:50:31 +0900

    Style: IssueCard 컴포넌트 css ellipsis 속성 추가

--- src/components/IssueCard/IssueCard.style.ts
+++ src/components/IssueCard/IssueCard.style.ts
@@ -5,7 +5,8 @@ export const Card = styled.li`
   justify-content: space-between;
   align-items: center;
   height: 6rem;
-  border: 1px solid black;
+  padding: 0 10px;
+  border-bottom: 1px solid black;
   transition: all 0.4s;
   cursor: pointer;
 
@@ -15,9 +16,13 @@ export const Card = styled.li`
 `;
 
 export const LeftSide = styled.section`
+  flex-basis: 90%;
   display: flex;
   flex-direction: column;
   gap: 10px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 
   & > div > span {
     margin-right: 10px;
@@ -25,11 +30,13 @@ export const LeftSide = styled.section`
 `;
 
 export const MainInfos = styled.div`
-  display: flex;
   font-size: 1.2rem;
   font-weight: bold;
 
   & > h3 {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
     font-size: 1.2rem;
   }
 `;
@@ -37,9 +44,13 @@ export const MainInfos = styled.div`
 export const SubInfos = styled.div`
   font-size: 1rem;
   color: #363535;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 `;
 
 export const Comment = styled.div`
+  margin-left: 10px;
   font-size: 1rem;
   color: #363535;
 `;
